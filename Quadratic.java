988a05f321102ae22ec21fbad05d2542f4ecaa06
kdjohnson <kdjohnson@users.noreply.github.com>
2015-03-07 17:24:55 -0500

    Update Quadratic.java
    A whole bunch of changes mostly in solveQuadratic().  Mostly having checks for situations that involve the discriminant. Lets say the a = 0 and b = 2 and c = 9.  The discriminant would be 2, which would satisfy hasDistinctRealRoots(). However if you did the quadratic equation you would get 0, which would be wrong. So I have a check for if a == 0. which based on different situations of a being positive/negative or c being positive/negative that will give an appropriate answer. I tested it and so far everything seems fine. I think that we need to just tidy up how the roots are presented but like I tested a = 2, b = 0, c = 9 and it gives a correct answer but it isn't formatted for a certain number of decimals. The equation prints fine but like I said the answers for decimals are not formatted yet.  So formatting and just testing all the methods is whats left but I feel most of the hard work is done.

--- Quadratic.java
+++ Quadratic.java
@@ -1,4 +1,4 @@
-package Quadratic;
+package Homework2;
 
  class Quadratic {
     private int a, b, c;
@@ -11,10 +11,6 @@ package Quadratic;
         setC( newC );
     }
     
-    /**
-     * @return int - the discriminant based off of the 
-     * values of variables a, b, and c.
-    */
     private int discriminant( )
     {
         return ( ( b * b ) - 4 * a * c );
@@ -55,20 +51,14 @@ package Quadratic;
         return comment;
     }
     
-    /**
-     * @return boolean - whether the equation has Distinct Real Roots or not based on whether the discriminant is > 0 
-    */
     public boolean hasDistinctRealRoots( )
     {
-       return discriminant( ) > 0;
+       return discriminant() > 0;
     }
     
-    /**
-     * @return boolean - whether the equation has Double Real Roots or not based on whether the discriminant is equal to 0 
-    */
     public boolean hasDoubleRealRoots( )
     {
-        return discriminant( ) == 0;
+        return discriminant() == 0;
     }
     
     public void setA( int newA )
@@ -96,17 +86,79 @@ package Quadratic;
                     a Complex object for each root. Which is then used to create
                     a ComplexPair object that has both real roots for the return. 
                 */
+               if( a == 0 )
+                    {
+                        root1 = Math.negateExact( c ) / (double) b;             
+                        Complex num1 = new Complex(root1, 0);
+                        Complex num2 = num1; 
+                                    
+                        ComplexPair num3 = new ComplexPair(num1, num2);
+                        return num3;
+                    }
+                 else if( b == 0)
+                    {
+                        if( a > 0 )
+                            {
+                                if(c > 0)
+                                    {                                            
+                                        root1 = -b + Math.sqrt( Math.abs( discriminant() ) ) / (2 * a);
+                                        root2 = -b - Math.sqrt( Math.abs( discriminant() ) ) / (2 * a);
+                                        Complex num1 = new Complex(0, root1);
+                                        Complex num2 = new Complex(0, root2);
+                                            
+                                        ComplexPair num3 = new ComplexPair(num1, num2);
+                                        return num3; 
+                                    }
+                                
+                                else
+                                   {
+                                        root1 = -b + Math.sqrt( Math.abs( discriminant() ) ) / (2 * a);
+                                        root2 = -b - Math.sqrt( Math.abs( discriminant() ) ) / (2 * a);
+                                        Complex num1 = new Complex(0, root1);
+                                        Complex num2 = new Complex(0, root2);
+                                            
+                                        ComplexPair num3 = new ComplexPair(num1, num2);
+                                        return num3; 
+                                   }
+ 
+                            }
+                        
+                        else if( a < 0)
+                            {
+                                if(c > 0)
+                                    {                                
+                                        root1 = ( -b + Math.sqrt( discriminant() ) ) / (2 * a);
+                                        root2 = ( -b - Math.sqrt( discriminant() ) ) / (2 * a); 
+                                        Complex num1 = new Complex(root1, 0);
+                                        Complex num2 = new Complex(root2, 0);
+                                                    
+                                        ComplexPair num3 = new ComplexPair(num1, num2);
+                                        return num3; 
+                                    }
+                                
+                                    else
+                                        {
+                                            root1 = -b + Math.sqrt( Math.abs( discriminant() ) ) / (2 * a);
+                                            root2 = -b - Math.sqrt( Math.abs( discriminant() ) ) / (2 * a);
+                                            Complex num1 = new Complex(0, root1);
+                                            Complex num2 = new Complex(0, root2);
+                                        
+                                            ComplexPair num3 = new ComplexPair(num1, num2);
+                                            return num3;            
+                                        }
+                                }
+                    }
+               
                 root1 = ( (-b + Math.sqrt( discriminant() ) ) / (2 * a) );
                 root2 = ( (-b - Math.sqrt( discriminant() ) ) / (2 * a) );
                 
-                Complex num1 = new Complex(root1, 0);
-                Complex num2 = new Complex(root2, 0);
-                
+                Complex num1 = new Complex(root1, 0);        
+                Complex num2 = new Complex(root2, 0); 
                 ComplexPair num3 = new ComplexPair(num1, num2);
                 return num3;
                 
             }
-            else if( hasDoubleRealRoots() == true )
+            else if(hasDoubleRealRoots() == true)
                 {
                     /*
                         Creates an object that holds the double root for the
@@ -114,18 +166,80 @@ package Quadratic;
                         are the same. A Complex object was created for each root.
                         Which was used to create a ComplexPair object to return. 
                     */
-                    
+                   if( a == 0 )
+                    {
+                        root1 = Math.negateExact( c ) / (double) b; 
+                                    
+                        Complex num1 = new Complex(root1, 0);
+                        Complex num2 = num1; 
+                                    
+                        ComplexPair num3 = new ComplexPair(num1, num2);
+                        return num3;
+                    }
+                 else if( b == 0)
+                    {
+                        if( a > 0 )
+                            {
+                                if(c > 0)
+                                    {                                            
+                                        root1 = -b + Math.sqrt( Math.abs( discriminant() ) ) / (2 * a);
+                                        root2 = -b - Math.sqrt( Math.abs( discriminant() ) ) / (2 * a);
+                                        Complex num1 = new Complex(0, root1);
+                                        Complex num2 = new Complex(0, root2);
+                                            
+                                        ComplexPair num3 = new ComplexPair(num1, num2);
+                                        return num3; 
+                                    }
+                                
+                                else
+                                   {
+                                        root1 = -b + Math.sqrt( Math.abs( discriminant() ) ) / (2 * a);
+                                        root2 = -b - Math.sqrt( Math.abs( discriminant() ) ) / (2 * a);
+                                        Complex num1 = new Complex(0, root1);
+                                        Complex num2 = new Complex(0, root2);
+                                            
+                                        ComplexPair num3 = new ComplexPair(num1, num2);
+                                        return num3; 
+                                   }
+ 
+                            }
+                        
+                        else if( a < 0)
+                            {
+                                if(c > 0)
+                                    {                                
+                                        root1 = ( -b + Math.sqrt( discriminant() ) ) / (2 * a);
+                                        root2 = ( -b - Math.sqrt( discriminant() ) ) / (2 * a); 
+                                        Complex num1 = new Complex(root1, 0);
+                                        Complex num2 = new Complex(root2, 0);
+                                                    
+                                        ComplexPair num3 = new ComplexPair(num1, num2);
+                                        return num3; 
+                                    }
+                                
+                                    else
+                                        {
+                                            root1 = -b + Math.sqrt( Math.abs( discriminant() ) ) / (2 * a);
+                                            root2 = -b - Math.sqrt( Math.abs( discriminant() ) ) / (2 * a);
+                                            Complex num1 = new Complex(0, root1);
+                                            Complex num2 = new Complex(0, root2);
+                                        
+                                            ComplexPair num3 = new ComplexPair(num1, num2);
+                                            return num3;            
+                                        }
+                                }
+                    }
                     root1 = ( (-b + Math.sqrt( discriminant() ) ) / (2 * a) );
                     root2 = root1;
                 
-                    ComplexPair num4 = new ComplexPair(root1, 0);
-                    //ComplexPair num5 = new Complex(root2, 0); 
+                    Complex num1 = new Complex(root1, 0);
+                    Complex num2 = new Complex(root2, 0); 
                 
-                    ComplexPair num6 = new ComplexPair(num4, num5);
-                    return num6; 
+                    ComplexPair num3 = new ComplexPair(num1, num2);
+                    return num3; 
                 }
-                else
-                {   /*
+                else if(discriminant() < 0) 
+                    {   /*
                         This is for if the discriminant is a negative number or
                         complex. I used Math.abs to convert the discriminant to 
                         a positive number. So that the imaginary number is still
@@ -133,27 +247,94 @@ package Quadratic;
                     
                         Then I put each root into their own Complex object. With
                         those Complex objects I created a ComplexPair. 
-                    */
-                       root1 = ( (-b + Math.sqrt( Math.abs( discriminant() ) ) ) / (2 * a) );
-                       root2 = ( (-b + Math.sqrt( Math.abs( discriminant() ) ) ) / (2 * a) );
+                         */
+                        if( a == 0 )
+                            {
+                                root1 = Math.negateExact( c ) / (double) b; 
+                                    
+                                Complex num1 = new Complex(root1, 0);
+                                Complex num2 = new Complex(0, 0); 
+                                    
+                                ComplexPair num3 = new ComplexPair(num1, num2);
+                                return num3;
+                            }
+                        else if( b == 0)
+                            {
+                                if( a > 0 )
+                                    {
+                                        if(c > 0)
+                                            {                                            
+                                                root1 = -b + Math.sqrt( Math.abs( discriminant() ) ) / (2 * a);
+                                                root2 = -b - Math.sqrt( Math.abs( discriminant() ) ) / (2 * a);
+                                                Complex num1 = new Complex(0, root1);
+                                                Complex num2 = new Complex(0, root2);
+                                            
+                                                ComplexPair num3 = new ComplexPair(num1, num2);
+                                                return num3; 
+                                            }
+                                
+                                        else
+                                            {
+                                                root1 = -b + Math.sqrt( Math.abs( discriminant() ) ) / (2 * a);
+                                                root2 = -b - Math.sqrt( Math.abs( discriminant() ) ) / (2 * a);
+                                                Complex num1 = new Complex(0, root1);
+                                                Complex num2 = new Complex(0, root2);
+                                            
+                                                ComplexPair num3 = new ComplexPair(num1, num2);
+                                                return num3; 
+                                            }
+                                    }
+                        
+                                else if( a < 0)
+                                    {
+                                        if(c > 0)
+                                            {                                
+                                                root1 = ( -b + Math.sqrt( discriminant() ) ) / (2 * a);
+                                                root2 = ( -b - Math.sqrt( discriminant() ) ) / (2 * a); 
+                                                Complex num1 = new Complex(root1, 0);
+                                                Complex num2 = new Complex(root2, 0);
+                                                    
+                                                ComplexPair num3 = new ComplexPair(num1, num2);
+                                                return num3; 
+                                            }
+                                
+                                        else
+                                            {
+                                                root1 = -b + Math.sqrt( Math.abs( discriminant() ) ) / (2 * a);
+                                                root2 = -b - Math.sqrt( Math.abs( discriminant() ) ) / (2 * a);
+                                                Complex num1 = new Complex(0, root1);
+                                                Complex num2 = new Complex(0, root2);
+                                        
+                                                ComplexPair num3 = new ComplexPair(num1, num2);
+                                                return num3;            
+                                            }
+                                    }
+                            }
+                        
+                        root1 = ( (-b + Math.sqrt( Math.abs( discriminant() ) ) ) / (2 * a) );
+                        root2 = ( (-b - Math.sqrt( Math.abs( discriminant() ) ) ) / (2 * a) );
                        
-                       Complex num7 = new Complex(0, root1);
-                       Complex num8 = new Complex(0, root2);
+                        Complex num1 = new Complex(0, root1);
+                        Complex num2 = new Complex(0, root2);
                        
-                       ComplexPair num9 = new ComplexPair( num7, num8);
-                       return num9; 
-                }
-        
-            
-    }
+                        ComplexPair num3 = new ComplexPair( num1, num2);
+                        return num3; 
+                    }
+                        
+        root1 = 0.0;
+        root2 = root1;
+        Complex num1 = new Complex(root1, root2);
+        Complex num2 = num1;
+        ComplexPair num3 = new ComplexPair(num1, num2);
+        return num3; 
+    } //end solveQuadratic 
     
     @Override
     public String toString( )
     {
-        return "Number a is: " + getA( )
-                + "Number b is: " + getB( )
-                + "Number c is: " + getC( )
-                + "The comment is: " + getComment( );
+        return "Equation: " + (a == 1 ? "x^2" : (a == -1 ? "-x^2" : (a == 0 ? "" : a + "x^2") ) ) + 
+                (b == 1 ? "x" : (b == -1 ? "-x" : (b > 0 ? " + " + b + "x" : (b == 0 ? "" : b + "x") ) ) )
+                + " " + (c > 0 ? "+ " + c : c); 
     }
     
     
